77d7582e5b7f007cabaac68ebac24b2ba5c9c824
Marviel <lukebechtel4@gmail.com>
2023-05-09 08:32:38 -0400

    Add isTypedUuidv4

--- src/functions/typedUuidV4.ts
+++ src/functions/typedUuidV4.ts
@@ -1,4 +1,40 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, validate as isUuid } from 'uuid';
+
+export type TypedUuidV4<TTypeName extends string = string> =
+  `${TTypeName}_${string}`;
+
+export function isTypedUuidV4<TTypeName extends string = string>(
+  value: unknown,
+  typeName?: TTypeName
+): value is TypedUuidV4<TTypeName> {
+  // Split on the first `_` character
+  const parts = typeof value === 'string' ? value.split('_') : [];
+
+  // If we didn't find a typeName and a UUID, it's not a typed UUID.
+  if (parts.length !== 2) {
+    return false;
+  }
+
+  const foundTypeName = parts[0];
+  const foundUuid = parts[1];
+
+  // If the typeName was passed in, make sure the type name matches it.
+  if (typeName !== undefined && foundTypeName !== typeName) {
+    return false;
+  }
+
+  // Make sure we found a type name.
+  if (foundTypeName.length < 1) {
+    return false;
+  }
+
+  // Make sure it's actually a UUID
+  if (!isUuid(foundUuid)) {
+    return false;
+  }
+
+  return true;
+}
 
 export function typedUuidV4<TTypeName extends string = any>(
   typeName: TTypeName
