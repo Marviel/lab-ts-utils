464fbdc8ab552c205c0cc608efa475b58665cdda
Marviel <lukebechtel4@gmail.com>
2023-05-28 14:16:51 -0400

    Add trimLines

--- src/functions/trimLines.ts
+++ src/functions/trimLines.ts
@@ -1,56 +1,78 @@
 /**
  * Trims the whitespace from the start and end of each line.
- * 
+ *
  * Also optionally trims the leading and trailing line blocks.
- * 
+ *
  * @param str The string to trim.
  * @param config { trimVerticalStart: boolean, trimVerticalEnd: boolean } Optional config object. Defaults to { trimVerticalStart: true, trimVerticalEnd: true } if not provided.
  *         trimVerticalStart: If true, removes the leading line blocks.
  *         trimVerticalEnd: If true, removes the trailing line blocks.
  * @returns The trimmed string.
  */
-export function trimLines(str: string, config?: { trimVerticalStart: boolean, trimVerticalEnd: boolean }): string {
-    const { trimVerticalStart, trimVerticalEnd } = {
-        trimVerticalStart: config?.trimVerticalStart ?? true,
-        trimVerticalEnd: config?.trimVerticalEnd ?? true
+export function trimLines(
+  str: string,
+  config?: { trimVerticalStart: boolean; trimVerticalEnd: boolean }
+): string {
+  const { trimVerticalStart, trimVerticalEnd } = {
+    trimVerticalStart: config?.trimVerticalStart ?? true,
+    trimVerticalEnd: config?.trimVerticalEnd ?? true,
+  };
+
+  const splitLines = str.split('\n');
+
+  let firstOccupiedLineIdx: number | undefined = undefined;
+  let lastOccupiedLineIdx: number | undefined = undefined;
+
+  // Trim the whitespace for each line.
+  const resultArrPreLeadTrail = splitLines.map((l, idx) => {
+    // Trim the whitespace for this line.
+    const newVal = l.trim();
+
+    // Bookkeeping
+    if (newVal.length > 0) {
+      if (firstOccupiedLineIdx === undefined) {
+        firstOccupiedLineIdx = idx;
+      }
+      lastOccupiedLineIdx = idx;
     }
 
-    const splitLines = str.split("\n");
+    return newVal;
+  });
 
-    let firstOccupiedLineIdx: number | null = null;
-    let lastOccupiedLineIdx: number | null = null;
+  let resultArr = resultArrPreLeadTrail;
 
-    // Trim the whitespace for each line.    
-    const resultArrPreLeadTrail = splitLines.map((l, idx) => {
-        // Trim the whitespace for this line.
-        const newVal = l.trim();
+  // Remove leading & trailing line blocks
+  if (trimVerticalStart && trimVerticalEnd) {
+    resultArr =
+      firstOccupiedLineIdx !== undefined && lastOccupiedLineIdx !== undefined
+        ? //@ts-ignore
+          resultArrPreLeadTrail.slice(
+            firstOccupiedLineIdx as number,
+            // @ts-ignore
+            (lastOccupiedLineIdx as number) + 1
+          )
+        : resultArrPreLeadTrail;
+  }
+  // Just remove leading
+  else if (trimVerticalStart) {
+    resultArr =
+      // @ts-ignore
+      firstOccupiedLineIdx !== undefined
+        ? // @ts-ignore
+          resultArrPreLeadTrail.slice(firstOccupiedLineIdx as number)
+        : resultArrPreLeadTrail;
+  }
+  // Just remove trailing
+  else if (trimVerticalEnd) {
+    resultArr =
+      // @ts-ignore
+      lastOccupiedLineIdx !== undefined
+        ? // @ts-ignore
+          resultArrPreLeadTrail.slice(0, (lastOccupiedLineIdx as number) + 1)
+        : // @ts-ignore
+          resultArrPreLeadTrail;
+  }
 
-        // Bookkeeping
-        if (newVal.length > 0) {
-            if (firstOccupiedLineIdx !== null) {
-                firstOccupiedLineIdx = idx;
-            }
-            lastOccupiedLineIdx = idx;
-        }
-
-        return newVal;
-    })
-
-    let resultArr = resultArrPreLeadTrail;
-
-    // Remove leading & trailing line blocks
-    if (trimVerticalStart && trimVerticalEnd) {
-        resultArr = (firstOccupiedLineIdx && lastOccupiedLineIdx) ? resultArrPreLeadTrail.slice(firstOccupiedLineIdx, lastOccupiedLineIdx + 1) : resultArrPreLeadTrail;
-    }
-    // Just remove leading
-    else if (trimVerticalStart) {
-        resultArr = (firstOccupiedLineIdx) ? resultArrPreLeadTrail.slice(firstOccupiedLineIdx) : resultArrPreLeadTrail;
-    }
-    // Just remove trailing
-    else if (trimVerticalEnd) {
-        resultArr = (lastOccupiedLineIdx) ? resultArrPreLeadTrail.slice(0, lastOccupiedLineIdx + 1) : resultArrPreLeadTrail;
-    }
-
-    // Join lines together and done.
-    return resultArr.join("\n");
+  // Join lines together and done.
+  return resultArr.join('\n');
 }
